f99c44d9bae8d1d13844a2fc5bf6e3335e4ab6b7
SeaSon2002 <61407079+SeaSon2002@users.noreply.github.com>
2022-05-31 15:46:40 +0700

    transslate

--- Types/Color.cs
+++ Types/Color.cs
@@ -2,10 +2,10 @@ namespace UNO.Types
 {
     public enum Color
     {
-        Red,
-        Green,
-        Blue,
-        Yellow,
+        Đỏ,
+        Lục,
+        Dương,
+        Vàng,
         None
     }
 }
